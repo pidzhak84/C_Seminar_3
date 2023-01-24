f2b567975a0bf1d2dac2bb26603f417fbdbe6259
pidzhak <pidzhak84@gmail.com>
2023-01-24 19:00:24 +0300

    Ex_18

---
+++ Ex_3_18/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Прогоамму, которая по заданному номеру четверти показывает диапазон возможных координат в этой четверти (x и y)
+*/
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+string getRageFromQuatre(int quarter)
+{
+    string result = $"Диапазон значений в {quarter} четверти";
+    switch (quarter)
+    {
+        case 1:
+            {
+                result += " x>0 и y>0 ";
+                break;
+            }
+        case 2:
+            {
+                result += " x<0 и y>0 ";
+                break;
+            }
+        case 3:
+            {
+                result += " x<0 и y<0 ";
+                break;
+            }
+        case 4:
+            {
+                result += " x>0 и y<0 ";
+                break;
+            }
+        default:
+            {
+                result = $"Четверти {quarter} не существует";
+                break;
+            }
+    }
+    return result;
+}
+
+int quarter = getUserValue("Введите номер четверти ");
+string rangeDescription=getRageFromQuatre(quarter);
+Console.WriteLine(rangeDescription);
